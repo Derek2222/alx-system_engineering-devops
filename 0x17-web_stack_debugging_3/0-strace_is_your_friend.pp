1af08099d35cdfc7979d1dfbeb3a2cc7be942eee
Ademola Emmanuel <111035925+Derek2222@users.noreply.github.com>
2023-06-23 14:43:30 -0700

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Puppet manifest to fix a 500 error in WordPress by replacing 'phpp' with 'php' in wp-settings.php file.
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-exec { 'fixed-phpp':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => '/bin';
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
